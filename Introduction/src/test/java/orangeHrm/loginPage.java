16e6c51542f01dfd1cb60de9f30b5b6ddf188101
Amritsharma2897 <amrit.sharma@clanap.com>
2024-10-21 12:16:09 +0530

    Updated test script

---
+++ Introduction/src/test/java/orangeHrm/loginPage.java
@@ -0,0 +1,25 @@
+package orangeHrm;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		driver.quit();
+	}
+
+}
